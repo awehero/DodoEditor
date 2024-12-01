6e44588f6ad57437bc7aa65f56d881b8d0676a6a
awehero <147273991+awehero@users.noreply.github.com>
2024-11-30 21:53:24 -0500

    Update Sidebar.Project.App.js

--- editor/js/Sidebar.Project.App.js
+++ editor/js/Sidebar.Project.App.js
@@ -398,7 +398,14 @@ function SidebarProjectApp( editor ) {
 			let mesh;
 			switch (type) {
 				case 'A':
-					
+					const geometry = new THREE.PlaneGeometry( 1, 1, 1, 1 );
+					const material = new THREE.MeshStandardMaterial();
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'Plane';
+					mesh.material.side = THREE.DoubleSide;
+					mesh.position.y = -20;
+					mesh.rotation.x = 1.57079633;
+					mesh.scale.x = 1000;
 					break;
 				case 'B':
 					const geometry = new THREE.BoxGeometry( 1, 1, 1, 1, 1, 1 );
@@ -412,19 +419,54 @@ function SidebarProjectApp( editor ) {
 					} );
 					break;
 				case 'C':
-					
+					const geometry = new THREE.ConeGeometry( .5, 1, 16 );
+					const material = new THREE.MeshBasicMaterial( { color: 0xD52B2B } );
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'Cone';
+					mesh.userData.CustomTexture = [ 'hex', 'D52B2B', 1.0 ];
 					break;
 				case 'D':
-					
+					const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
+					const material = new THREE.MeshBasicMaterial( {
+						color: 0x24fc03,
+						opacity: 0.5,
+						transparent: true,
+					} );
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'End';
+					mesh.userData.CustomTexture = [ 'hex', '24fc03', 0.5 ];
 					break;
 				case 'E':
-					
+					const geometry = new THREE.CylinderGeometry( .5, .5, 1, 16, 1, false, 0, Math.PI * 2 );
+					const material = new THREE.MeshBasicMaterial( {
+						color: 0x0000ff,
+						opacity: 0.8,
+						transparent: true,
+					} );
+			
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'Cylinder';
+					mesh.userData.CustomTexture = [ 'hex', '0000ff', 0.8 ];
 					break;
 				case 'F':
-					
+					const geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI * 2, 0, Math.PI );
+					const material = new THREE.MeshBasicMaterial( {
+						color: 0x0000ff,
+						opacity: .8,
+						transparent: true,
+					} );
+			
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'Sphere';
+					mesh.userData.CustomTexture = [ 'hex', '0000ff', 0.8 ];
+			
+					editor.execute( new AddObjectCommand( editor, mesh ) );
 					break;
 				case 'G':
-					
+					const geometry = new THREE.OctahedronGeometry( 3, 1 );
+					const material = new THREE.MeshStandardMaterial();
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.name = 'Monkey';
 					break;
 				default:
                                         console.error('Unknown object type:', type);
